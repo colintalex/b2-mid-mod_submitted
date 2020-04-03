8b8fb4888fcff4e81058c0923bb4f108bd29975b
Colin Alexander <colintalex@gmail.com>
2020-04-03 11:58:40 -0600

    Creates Mech Show and Update action

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,19 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:mechanic_id])
+  end
+
+  def update
+    mechanic = Mechanic.find(params[:mechanic_id])
+    mechanic.assign_to_ride(strong_params[:ride_id])
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
+
+  private
+  def strong_params
+    params.permit(:ride_id, :mechanic_id)
+  end
 end
